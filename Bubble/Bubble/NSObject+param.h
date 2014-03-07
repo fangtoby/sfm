3ea40bc2e92bb2f83a12f39461e9348f2b4ba040
fang.yanliang <fang.yanliang@sh.adways.com>
2014-03-07 13:26:31 +0800

    add prams , util class

---
+++ Bubble/Bubble/NSObject+param.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+param.h
+//  Bubble
+//
+//  Created by 方燕良 on 3/7/14.
+//  Copyright (c) 2014 adways. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface  Param : NSObject
+
+@end
